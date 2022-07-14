8cd0d032f889ce875f2bc2596a29e3e2f07ff18f
AlexR2DSX <alexandrit2001@mail.ru>
2022-07-15 01:32:02 +0300

    нахождение мин и макс числа

---
+++ TASK2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите два числа");
+Console.Write("Первое число");
+double a=Convert.ToDouble(Console.ReadLine());
+Console.Write("Второе число");
+double b=Convert.ToDouble(Console.ReadLine());
+if(a>b)
+{
+Console.WriteLine("max="+Convert.ToString(a));
+Console.WriteLine("min="+Convert.ToString(b));
+}
+if(b>a)
+{
+Console.WriteLine("max="+Convert.ToString(b));
+Console.WriteLine("min="+Convert.ToString(a));
+}
+if(b==a)
+{
+    Console.WriteLine("Числа равны");
+}
