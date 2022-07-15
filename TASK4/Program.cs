cd100a23dcd05489f89f0d6953fdaff6e3a3aeff
AlexR2DSX <alexandrit2001@mail.ru>
2022-07-15 19:40:39 +0300

    нахожение четного числа

--- TASK4/Program.cs
+++ TASK4/Program.cs
@@ -5,12 +5,14 @@ Console.Write("Второе число");
 double b = Convert.ToDouble(Console.ReadLine());
 Console.Write("Третье число");
 double с = Convert.ToDouble(Console.ReadLine());
-if (a>b & a>c){
-Console.WriteLine("max="+Convert.ToString(a));
+  if (a>b & a>c){
+Console.WriteLine("max="+ Convert.ToString(a));
 }
-if (b>a & b>c){
-Console.WriteLine("max="+Convert.ToString(b));
+if (b>a & b>c)
+{
+Console.WriteLine("max="+ Convert.ToString(b));
 }
-if (c>a & c>c){
-Console.WriteLine("max="+Convert.ToString(c));
+if (c>a & c>b)
+{
+Console.WriteLine("max="+ Convert.ToString(c));
 }
---
+++ TASK6/Program.cs
@@ -0,0 +1,14 @@
+﻿class Task{
+static void Main()
+}
+Console.WritLine("Является ли число четным");
+Console.Write("Введите число");
+Double a=Convert .ToDouble(Console.RedLine());
+if (a%2==0)
+{
+    Console.WritLine("Да")
+}
+else
+{
+    Console.WritLine("Нет")
+}
