9d2f28692a082f7fa07603f1afdb73072c44e449
AlexR2DSX <alexandrit2001@mail.ru>
2022-07-15 19:07:40 +0300

    Нахождение максимального числа

---
+++ TASK4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Ппедите три числа");
+Console.Write("Первое число");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Второе число");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Третье число");
+double с = Convert.ToDouble(Console.ReadLine());
+if (a>b & a>c){
+Console.WriteLine("max="+Convert.ToString(a));
+}
+if (b>a & b>c){
+Console.WriteLine("max="+Convert.ToString(b));
+}
+if (c>a & c>c){
+Console.WriteLine("max="+Convert.ToString(c));
+}
